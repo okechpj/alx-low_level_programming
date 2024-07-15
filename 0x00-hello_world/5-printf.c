e5f3daf33694196dd52dd87011afa0d6d31858e8
peter <okechpj@gmail.com>
2024-07-15 06:30:46 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints with proper grammar, but the outcome
- * 	 is a piece of art, followed by a new line
+ * 	is a piece of art, followed by a new line
  *
  * return - Always 0
  */
@@ -11,5 +11,5 @@ int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
