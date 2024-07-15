016608e5c0126c599546d3fbeed05d0d5b1593ab
peter <okechpj@gmail.com>
2024-07-15 06:35:09 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints with proper grammar, but the outcome 
- * is a piece of art, followed by a new line
+ * main - Prints "with proper grammar, but the outcome 
+ * 		is a piece of art," followed by a new line
  *
  * return - Always 0
  */
